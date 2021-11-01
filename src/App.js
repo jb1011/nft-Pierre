3000ea12a7630d042aa667245385df1e4c848048
jb1011 <demenet.jb@hotmail.fr>
2021-11-01 23:40:33 +0100

    header ok

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { Redirect, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home'
+import About from './pages/About';
+import Gallery from './pages/Gallery';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/About" component={About} />
+      <Route exact path="/Gallery" component={Gallery} />
+      <Redirect to ="/" />
     </Switch>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 function Home() {
     return (
         <div>
-            <p>Hello</p>
+            <Navbar />
+            <Hero />
         </div>
     )
 }
